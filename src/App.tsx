f40d4ef79e84ec3a63460b3479b8c02170e88d4c
Davi Henrique <63513686+davihr1@users.noreply.github.com>
2023-05-13 18:06:39 -0300

    components: Header & Summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/theme/defaulTheme";
 import { GlobalStayle } from "./styles/global";
+import { Transactions } from "./pages/transactions";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStayle />
-      <h1>hello world</h1>
+      <Transactions />
     </ThemeProvider>
   )
 }
---
+++ src/pages/transactions/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/Heade";
+import { Summary } from "../../components/Summary";
+
+export function Transactions() {
+  return (
+    <div>
+      <Header />
+      <Summary />
+    </div>
+  )
+}
