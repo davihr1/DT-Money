f1fed7b7bd4b196b914be4a8d61cb0e143b5953a
Davi Henrique <63513686+davihr1@users.noreply.github.com>
2023-05-13 18:37:46 -0300

    Styled: Table transactions

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Heade";
 import { Summary } from "../../components/Summary";
+import { PriceHighlit, TransactionTable, TransactoinsContainer } from "./style";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+      <TransactoinsContainer>
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td width='50%' >Desenvolvimento de sites</td>
+              <td>
+                <PriceHighlit variant="income">
+                  R$ 10.000,00
+                </PriceHighlit>
+              </td>
+              <td>Venda</td>
+              <td>24/04/2023</td>
+            </tr>
+
+            <tr>
+              <td width='50%' >Suporte PC</td>
+              <td>
+                <PriceHighlit variant="outcome">
+                 - R$ 500,00
+                </PriceHighlit>
+              </td>
+              <td>Compra</td>
+              <td>24/04/2023</td>
+            </tr>
+          </tbody>
+        </TransactionTable>
+      </TransactoinsContainer>
     </div>
   )
 }
